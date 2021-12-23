9ca545af7d00f5c50862588587b16125c05fc94e
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-23 21:29:27 +0530

    Updated About

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,9 @@ export default function About(props) {
         <div className="container">
             <h2 className={`my-2 text-${props.theme==='light'?'dark':'light'}`}>About Us</h2>
             <p className={`text-${props.theme==='light'?'dark':'light'}`}>TextUtils is a word analyzer and manipulator, created in React.</p>
+            <p className={`text-${props.theme === 'light' ? 'dark' : 'light'}`}>
+                <a href="https://www.youtube.com/playlist?list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt" target="_blank" rel="noreferrer" className={`text-${props.theme === 'dark' ? 'light' : 'dark'}`}>CodeWithHarry ke react videos</a> ko dekh ke banaya hai. Chhap kar nahi, samajh kar.
+                </p>
             <a href="https://github.com/adarshpand3y/textutils-react" rel="noreferrer" target="_blank" className={`text-${props.theme === 'dark' ? 'light' : 'dark'}`}>Github Repository Link</a>
         </div>
     )
