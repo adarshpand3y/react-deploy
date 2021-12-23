7a3fe8a6cdd83ab4bad421401b87559306426dab
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-23 16:40:19 +0530

    Setup Router and made changes to public html and favicon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm.js';
-// import About from './components/About';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [alert, setAlert] = useState("");
   const [theme, setTheme] = useState('light');
 
   const switchTheme = (event) => {
-    if(theme === 'light') {
+    if (theme === 'light') {
       setTheme('dark');
       document.body.style.backgroundColor = '#333';
+      document.title = 'TextUtils - Home (Dark Mode)'
     }
     else {
       setTheme('light');
       document.body.style.backgroundColor = '#fff';
+      document.title = 'TextUtils - Home (Light Mode)'
     }
   }
 
@@ -29,12 +37,20 @@ function App() {
 
   return (
     <>
-      <Navbar changeTheme={switchTheme} theme={theme}/>
-      <div style={{minHeight:'60px'}}>
-        <Alert alertMessage={alert} />
-      </div>
-      <TextForm alertFunc={showAlert} theme={theme}/>
-      {/* <About theme={theme}/> */}
+      <Router>
+        <Navbar changeTheme={switchTheme} theme={theme} />
+        <div style={{ minHeight: '60px' }}>
+          <Alert alertMessage={alert} />
+        </div>
+        <Switch>
+          <Route exact path="/about">
+            <About theme={theme} />
+          </Route>
+          <Route exact path="/">
+            <TextForm alertFunc={showAlert} theme={theme} />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
