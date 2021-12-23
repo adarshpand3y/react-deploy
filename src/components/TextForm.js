1160b0ea9f7862dc6e84b46bdd4b057120725f9b
Adarsh Pandey <mail.apandey2001@gmail.com>
2021-12-23 16:51:10 +0530

    Minor bog fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -59,22 +59,22 @@ export default function TextForm(props) {
               style={props.theme==='light'?{color: '#212529'}:{color: '#fff'}}
             ></textarea>
           </div>
-          <button type="button" className="btn btn-primary m-2" onClick={handleUpCLick}>
+          <button disabled={text.length===0} type="button" className="btn btn-primary m-2" onClick={handleUpCLick}>
             Convert to Uppercase
           </button>
-          <button type="button" className="btn btn-primary m-2" onClick={handleDownCLick}>
+          <button disabled={text.length===0} type="button" className="btn btn-primary m-2" onClick={handleDownCLick}>
             Convert to Lowercase
           </button>
-          <button type="button" className="btn btn-primary m-2" onClick={handleRemoveSpaces}>
+          <button disabled={text.length===0} type="button" className="btn btn-primary m-2" onClick={handleRemoveSpaces}>
             Remove All Spaces
           </button>
-          <button type="button" className="btn btn-primary m-2" onClick={handleRemoveExtraSpaces}>
+          <button disabled={text.length===0} type="button" className="btn btn-primary m-2" onClick={handleRemoveExtraSpaces}>
             Remove Extra Spaces
           </button>
-          <button type="button" className="btn btn-primary m-2" onClick={handleCopyToClipboard}>
+          <button disabled={text.length===0} type="button" className="btn btn-primary m-2" onClick={handleCopyToClipboard}>
             Copy Text to Clipboard
           </button>
-          <button type="button" className="btn btn-danger m-2" onClick={handleClearTextarea}>
+          <button disabled={text.length===0} type="button" className="btn btn-danger m-2" onClick={handleClearTextarea}>
             Clear Workspace
           </button>
         </div>
@@ -83,7 +83,7 @@ export default function TextForm(props) {
           <p className={`text-${props.theme==='light'?'dark':'light'}`}>{text.split(" ").filter((element) => {return element.length!==0}).length} word(s) and {text.length} character(s).</p>
           <p className={`text-${props.theme==='light'?'dark':'light'}`}>{0.008*text.split(" ").filter((element) => {return element.length!==0}).length} minutes to speak.</p>
           <h3 className={`text-${props.theme==='light'?'dark':'light'}`}>Preview</h3>
-          <p className={`text-${props.theme==='light'?'dark':'light'}`}>{text?text:"Please write some text to preview."}</p>
+          <p className={`text-${props.theme==='light'?'dark':'light'}`}>{text?text:"Nothing to preview!"}</p>
         </div>
       </>
     );
